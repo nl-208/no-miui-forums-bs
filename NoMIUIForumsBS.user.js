71809356435a0a4c644e0a04c056242e17ec4c0a
Nate L <info@theautomatedparts.com>
2018-07-06 15:48:34 -0400

    Rewrite part 1.
    Moved variables to the top, split the file into different functions, as opposed to the unholy block we had before.
    
    Next step is to have the functions fire right when the elements load, so the annoyances will be killed as soon as they come on screen.

--- NoMIUIForumsBS.user.js
+++ NoMIUIForumsBS.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @id com.theautomatedparts.nlivingston.nomiuiforumsbs
 // @name         No MIUI Forums BS
-// @version      1.1
+// @version      1.2
 // @homepage https://theautomatedparts.com
 // @description  Removes nonsense (medals, Chinese characters in thread titles, stamps, smileys are shrunk down, less icons) from the MIUI forum.
 // @author       NLivingston
@@ -12,38 +12,44 @@
 // @grant        GM_addStyle
 // @run-at document-end
 // ==/UserScript==
+// Variables
+var imgsOnPage = document.getElementsByTagName('img');
+var threadsInList = document.GetElementsByClassName('s xst'); {
 
-(function(cleanUpPage) {
-    'use strict';
-    // get every image on the page and put it in an array.
-    var imgsOnPage = document.getElementsByTagName('img');
-    for (var i = 0; i < imgsOnPage.length; i++) {
-        // check if the image is from the smileys directory. if it is, make it so it's not quite so bloody huge.
-        if (imgsOnPage[i].src.match('^http://en.miui.com/static/image/smiley/') || imgsOnPage[i].src.match('http://static.en.intl.miui.com/static/image/smiley')) {
-            imgsOnPage[i].setAttribute("height", "20");
-            imgsOnPage[i].setAttribute("width", "20");
-        }
-        // same as before, but check to see if it's from the stamps directory. if so, remove it.
-        else if (imgsOnPage[i].src.match('^http://static.en.intl.miui.com/static/image/stamp')){
-            $( imgsOnPage[i] ).remove();
-        }
-    }
-    // remove the medal boxes
-    // get all users' medal boxes and put them in an array.
-    var medalBoxes = document.getElementsByClassName('md_ctrl');
-    for (var j = 0; j < medalBoxes.length; i++) {
-        // ...then remove them.
-        $( medalBoxes[j] ).remove();
-    }
-    // you know the drill. remove all threads with Chinese characters in the title from the list.
-    var threadsInList = document.GetElementsByClassName('s xst'); {
-        for (var k = 0; k < threadsInList.length; i++) {
-            // something something remove the entries
-            if (threadsInList[k].innerHTML.match(/[\u3400-\u9FBF]/)) {
-                $( threadsInList[k] ).remove();
+(function(removeMedals) {
+        // remove the medal boxes
+        // get all users' medal boxes and put them in an array.
+        var medalBoxes = document.getElementsByClassName('md_ctrl');
+        for (var j = 0; j < medalBoxes.length; i++) {
+            // ...then remove them.
+            $(medalBoxes[j]).remove();
+        })
+
+    (function(fixSmileys) {
+            for (var i = 0; i < imgsOnPage.length; i++) {
+                // check if the image is from the smileys directory. if it is, make it so it's not quite so bloody huge.
+                if (imgsOnPage[i].src.match('^http://en.miui.com/static/image/smiley/') || imgsOnPage[i].src.match('http://static.en.intl.miui.com/static/image/smiley')) {
+                    imgsOnPage[i].setAttribute("height", "20");
+                    imgsOnPage[i].setAttribute("width", "20");
+                }
+            })
+
+        (function(removeStamps) {
+            // same as before, but check to see if it's from the stamps directory. if so, remove it.
+            if (imgsOnPage[i].src.match('^http://static.en.intl.miui.com/static/image/stamp')) {
+                $(imgsOnPage[i]).remove();
             }
-        }
-    }
-})
+        })
 
-();
+        (function(removeChineseTitles) {
+            // you know the drill. remove all threads with Chinese characters in the title from the list.
+                for (var k = 0; k < threadsInList.length; i++) {
+                    // remove the entries from the list
+                    if (threadsInList[k].innerHTML.match(/[\u3400-\u9FBF]/)) {
+                        $(threadsInList[k]).remove();
+                    }
+                }
+            }
+        })
+
+        ();
